9afced1081d8c82f750256431215b15b2ebafb50
voider755 <76598729+voider755@users.noreply.github.com>
2020-12-27 00:05:14 +0100

    Add files via upload

--- almh.py
+++ almh.py
@@ -5,86 +5,86 @@
 # License: FreeBSD License. Use at your own risk
 # Gives you a menu with some common maintenance options inside a *stock* Arch Linux system
 # Plausibly works on derivatives too, but not tested at the moment. 
-# Pacman is absolutely needed, pacman-contrib is needed for some options.
-# Please read the README file and code before using this 
+# Pacman is absolutely needed, pacman-contrib and newsboat are needed for some options.
+# Please read the README file and code before using this
 green = '\033[32m'
 cyan = '\033[36m'
 red = '\033[31m'
+blue = '\033[34m'
 resetc = '\33[m'
-print(green + '************************************************************************' , resetc)
-print(green + '**' , resetc , '                            almh                                 ' , green + '**' , resetc)
-print(green + '**' , resetc , '                (ARCH LINUX MAINTENANCE HELPER)                  ' , green + '**' , resetc)
-print(green + '************************************************************************' , resetc)
+print('\n' , red + 'IMPORTANT.', resetc + "If you are using this for the first time, you'll need")
+print ("newsboat and pacman-contrib to use options 1, 3 and 7 .")
+print('Type special option' , green + "'12'" , resetc + 'to install pacman-contrib.')
+print('Type special option' , green + "'13'" , resetc + "to install newsboat and add Arch news RSS.")
+print('to read the Arch web news section before upgrading.\n')
+print(blue + '************************************************************************' , resetc)
+print(blue + '**' , resetc , '                            almh                                 ' , blue + '**' , resetc)
+print(blue + '**' , resetc , '                (ARCH LINUX MAINTENANCE HELPER)                  ' , blue + '**' , resetc)
+print(blue + '************************************************************************' , resetc)
 print('\n')
-print("PLEASE CHOOSE AN OPTION (type a number 1-13):\n***Options xx require that you have pacman-contrib installed.\nEntries marked with '#' will run with sudo.\nWrite your password if, and when, prompted***\n")
+print("PLEASE CHOOSE AN OPTION (type a number 1-11):\n***Options xx require that you have pacman-contrib installed.\nEntry '1' require that you have installed and configured newsboat.\nEntries marked with '#' will run with sudo.\nWrite your password if, and when, prompted***\n")
 import os
 while True:
     try:
         print(cyan + """************************************************************************
-* 1.  # Update and upgrade your system                                 *
-* 2.  # Update and upgrade your system (verbose mode)                  *
-* 3.  # Clean downloaded packages cache                                *
-* 4.  # Remove orphaned packages                                       *
-* 5.  Search packages in the repositories database                     *
-* 6.  # Install packages                                               *
-* 7   # Remove installed package(s) (with unneeded dependencies)       *
-* 8.  List all installed packages                                      *
+* 1.  Read the Arch web news section before upgrading your system      *
+* 2.  # Update and upgrade your system                                 *
+* 3.  # Remove all cached versions of installed and uninstalled        *
+      packages (except for the most recent 3 versions)                 *
+* 4.  Search packages in the repositories database                     *
+* 5.  # Install packages                                               *
+* 6   # Remove installed package(s) (with unneeded dependencies)       *
+* 7.  # Remove all cached versions of uninstalled packages             *
+* 8.  # Remove orphaned packages                                       *
 * 9.  List explicitly installed packages                               *
-* 10. Remove all cached versions of installed and uninstalled packages *
-      (Except for the most recent 3 versions)                          *
-* 11. # Remove all cached versions of uninstalled packages             *
-* 12. # Remove ALL old kernels (exits if no old kernels are found)     *
-* 13. Exit                                                             *
+* 10. List all installed packages                                      *
+* 11. Exit                                                             *
 ************************************************************************\n
  """ , resetc)
         options = int(input())
         if options == 1:
+            os.system("newsboat -r")
+        elif options == 2:
             os.system("sudo pacman -Syu")
             print("\n")
-        elif options == 2:
-            os.system("sudo xbps-install -Suv")
-            print("\n")
         elif options == 3:
-            os.system("sudo xbps-remove -Ov")
+            os.system("sudo paccache -rv")
             print("\n")
         elif options == 4:
-            os.system("sudo xbps-remove -ov")
-            print("\n")
-        elif options == 5:
             package = input('Type the package name or keyword of the package you want to query: ')
             os.system('pacman -Ss ' + package)
             print("\n")
-        elif options == 6:
+        elif options == 5:
             install = input('Type the package name(s) you want to install: ')
             os.system('sudo pacman -S ' + install)
             print('\n')
-        elif options == 7:
+        elif options == 6:
             remove = input('Type the name of the package(s) you want to remove from your system: ')
             os.system('sudo pacman -Rs ' + remove)
             print("\n")
+        elif options == 7:
+            os.system("sudo paccache -ruk0 -v")
+            print("\n")
         elif options == 8:
-            os.system("pacman -Ql")
+            os.system('sudo pacman -Qtdq | sudo pacman -Rns -')
             print("\n")
         elif options == 9:
-            os.system("pacman -Qe | column")
+            os.system('pacman -Qe | column')
             print("\n")
         elif options == 10:
-            #print("You can remove this/these old kernel versions:\n(Empty if no old kernels are found)")
-            os.system("sudo paccache -rv")
-            print("\n")
-        elif options == 11:
-			os.system("sudo paccache -ruk0 -v")
-            #vkpurge = input("Type the kernel version you want to remove:\n(Use option 10 before, type anything to exit back to menu) ")
-            #os.system("sudo vkpurge rm " + vkpurge)
+            os.system('pacman -Q')
             print("\n")
         elif options == 12:
-            os.system("sudo vkpurge rm all")
+            os.system('sudo pacman -S pacman-contrib')
             print("\n")
         elif options == 13:
-            print("Thank you for using vlmh")
+            os.system("sudo pacman -S newsboat && sleep 4 && mkdir ~/.newsboat/ && echo 'https://archlinux.org/feeds/news/' > ~/.newsboat/urls")
+            print("\n")
+        elif options == 11:
+            print("Thank you for using almh")
             break
         else:
-            print(red + "Please choose an option, 1-13\n" , resetc)
+            print(red + "Please choose an option, 1-11\n" , resetc)
     except:
-        print (red + "Please choose an option, 1-13\n" , resetc)
+        print (red + "Please choose an option, 1-11\n" , resetc)
 
